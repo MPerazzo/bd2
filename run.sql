34cd2da2979454d334bcefb00d3c8cdd7690ce11
jfrojo <jfrojo.92@gmil.com>
2017-10-25 11:55:21 -0300

    Added 	run.sql -> Automated script to run all scripts in one line 	changelog.txt -> To track what are we really changing on the script (only final version) 	explanation.txt -> To track changes and explain then (every change) 	backup.sql -> Renamed export.sql to backup.sql (DO NOT WORK ON THIS FILE) 	create.sql -> Our modified version of backup.sql 	script.sql -> Our script (old TP_script.sql)

---
+++ run.sql
@@ -0,0 +1,6 @@
+/*@/home/general/Downloads/bd2-master/drop.sql;
+@/home/general/Downloads/bd2-master/lapapota.sql;
+@/home/general/Downloads/bd2-master/script.sql;*/
+BEGIN
+	CONCILIATE_PKG.profiler();
+END;
