c30972c0b3ba5f17faee3a924b0427be72b6e3c2
matias <maperazzo@itba.edu.ar>
2017-10-27 03:35:05 -0300

    changes on tables, sequences, and others

--- run.sql
+++ run.sql
@@ -1,6 +1,5 @@
 /*@/home/general/Downloads/bd2-master/drop.sql;
-@/home/general/Downloads/bd2-master/lapapota.sql;
-@/home/general/Downloads/bd2-master/script.sql;*/
+@/home/general/Downloads/bd2-master/lapapota.sql;*/
 BEGIN
 	CONCILIATE_PKG.profiler();
 END;
